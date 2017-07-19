d04e7bb3a0b4d18722b29b33bbf192e35238c661
Mike Lohrman <m.lohrman@5byfivecreative.com>
2017-07-19 16:42:01 -0500

    More initial scss setup

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		jshint: {
+			dist: {
+				src: ['assets-src/js/*.js']
+			}
+		},
+		concat: {
+			dist: {
+				src: ['node_modules/gsap/TimelineMax.js', 'node_modules/gsap/TweenMax.js', 'node_modules/scrollmagic/scrollmagic/minified/ScrollMagic.min.js', 'node_modules/scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js', 'assets-src/js/thirdparty/*', 'assets-src/js/*'],
+				dest: 'assets/js/scripts.js'
+			}
+		},
+		uglify: {
+			dist: {
+				src: 'assets/js/scripts.js',
+				dest: 'assets/js/scripts.min.js'
+			}
+		},
+		sasslint: {
+			options: {
+				configFile: '.sass-lint.yml'
+			},
+			dist: {
+				src: ['assets-src/css/*']
+			}
+		},
+		sass: {
+			dist: {
+				src: ['assets-src/css/styles.scss'],
+				dest: 'assets/css/styles.css'
+			}
+		},
+		postcss: {
+			options: {
+				processors: [
+					require('autoprefixer')({browsers: 'last 2 versions'})
+				]
+			},
+			dist: {
+				src: ['assets/css/style.css']
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['assets-src/js/*'],
+				tasks: ['jshint', 'concat'/*, 'uglify'*/]
+			},
+			css: {
+				files: ['assets-src/css/*'],
+				tasks: ['sasslint', 'sass', 'postcss']
+			}
+		}
+	});
+	
+	require('load-grunt-tasks')(grunt);
+	grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'sass', 'postcss']);
+};
